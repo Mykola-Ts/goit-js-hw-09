f3664cc71af04dddac56322b1eb0e80bacbbf8db
Mykola Ts <tsybulskiyk@gmail.com>
2023-07-09 21:09:50 +0300

    All tasks completed

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import 'dist/notiflix-notify-aio-3.2.6.min.js'
 
 const selectors = {
   body: document.querySelector('body'),
