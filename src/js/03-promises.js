060d28eb985cc2a7391bbee7c22b00458b6c80b4
Mykola Ts <tsybulskiyk@gmail.com>
2023-07-09 21:17:45 +0300

    All tasks completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,6 +1,5 @@
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.6.min.js';
-import 'dist/notiflix-notify-aio-3.2.6.min.js';
 
 const selectors = {
   form: document.querySelector('.form'),
